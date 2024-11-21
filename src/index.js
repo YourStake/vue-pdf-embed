9dfab1b56e6d642b3c3437839b46c7bf26c721f0
Gershon Bialer <gershon.bialer@gmail.com>
2024-11-21 13:16:48 -0800

    Fix security issue.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import VuePdfEmbed from './vue-pdf-embed.vue'
 import { getDocument } from 'pdfjs-dist/legacy/build/pdf.js'
 
-VuePdfEmbed.getDocument = getDocument
+const originalGetDocument = getDocument
+VuePdfEmbed.getDocument = (src) => {
+  // If src is already a DocumentInitParameters object
+  if (src && typeof src === 'object' && !ArrayBuffer.isView(src)) {
+    return originalGetDocument({
+      ...src,
+      isEvalSupported: false
+    })
+  }
+  // If src is a string/URL/TypedArray/ArrayBuffer
+  return originalGetDocument({
+    url: src,
+    isEvalSupported: false
+  })
+}
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.VuePdfEmbed = VuePdfEmbed
